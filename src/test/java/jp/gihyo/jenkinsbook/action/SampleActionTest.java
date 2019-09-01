ba4126afc11c1f45f2a23570934d2f957fcecefc
curren49918102 <curren49918102@gmail.com>
2019-09-01 13:53:52 +0900

    Update SampleActionTest.java

--- src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
+++ src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
@@ -81,6 +81,7 @@ public class SampleActionTest {
 		SampleAction action = new SampleAction();
 		HttpServletRequest request = createMock(HttpServletRequest.class);
 
+		expect(request.getParameter("FirstName")).andReturn("firstName");
 		expect(request.getParameter("LastName")).andReturn("");
 
 		replay(request);
